2dd0175450f40b5e6a19551748a40aa464774d5b
Makai Sándor <arcdeus@pop-os.home>
2025-12-16 21:10:34 +0100

    updated setup.py

--- Grad-TTS/model/monotonic_align/setup.py
+++ Grad-TTS/model/monotonic_align/setup.py
@@ -1,11 +1,16 @@
-""" from https://github.com/jaywalnut310/glow-tts """
-
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
+import os
+
+# Get the directory where this setup.py file is located
+this_dir = os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name = 'monotonic_align',
-    ext_modules = cythonize("core.pyx"),
-    include_dirs=[numpy.get_include()]
+  name = 'monotonic_align',
+  ext_modules = cythonize(
+      os.path.join(this_dir, "core.pyx"),
+      language_level=3
+  ),
+  include_dirs=[numpy.get_include()]
 )
